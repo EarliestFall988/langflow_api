05c3e67d31af0c50a7df1640d31a7481ddb500c5
Taylor Howell <howelltaylor195@gmail.com>
2023-06-22 23:08:00 -0500

    rewrote the string parsing algrithm

--- Core/Program.fs
+++ Core/Program.fs
@@ -117,25 +117,105 @@ let fileLocation = @"C:\Users\WorkPC\Documents\Software Developement\Programming
 //let test3 = ExplodeDocument("1 + 2\n3 + 4\n5 + 6")
 //printfn ("test3: %A") test3
 
-let rec ExplodeLine (line: char list) (result: char list) : string =
+exception ValueNotUnderstoodException of string
+
+type ExplodeStatus =
+    | Start
+    | Semicolon
+    | Token
+    | ReadString
+    | Number
+    | Add
+    | Mult
+    | Div
+    | Subtract
+    | Letter
+    | Let
+    | Equal
+
+let rec ReadBack (data: char list) (result: char list) (amtLeft: int) : string =
+
+    //printfn "\t\t\trev: %A" data
+
+    match (data, amtLeft) with
+    | ([], x) -> result |> List.toArray |> System.String
+    | (h::t, x) -> 
+        //printfn "%A" h
+        if x >= 0 then ReadBack t (List.append result [h]) (amtLeft - 1)
+        else result |> List.toArray |> System.String
+
+
+let IsLet result : bool =
+    printfn "\t\tres: %A" result
+    let str = ReadBack result [] 4
+    printfn "\t\tstr: %A" str
+    if str = "let" then true 
+    else false
+
+///<summary>clean line and convert into tokens</summary>
+let rec ExplodeLine (line: char list) (result: char list) (status : ExplodeStatus) (lineNumber: int) (characterNumber: int) : string =
     match line with
     | head :: tail ->
-        let appendedResult = List.append result [head]
-        ExplodeLine tail result
+        if head = ' ' && status <> ExplodeStatus.ReadString then
+            if (IsLet result) then 
+                ExplodeLine tail (List.append result [head]) ExplodeStatus.Let lineNumber (characterNumber + 1) //keep the whitespace - we're now looking at some sort of value store
+            else
+                ExplodeLine tail result status lineNumber (characterNumber + 1)
+        elif Char.IsDigit head then
+            let appendedResult = List.append result [head]
+            //printfn "\tappended Result: %A" appendedResult
+            ExplodeLine tail appendedResult status lineNumber (characterNumber + 1)
+        elif status <> ExplodeStatus.ReadString then
+            if head = '+' then
+                ExplodeLine tail (List.append result [head]) ExplodeStatus.Add lineNumber (characterNumber + 1)
+            elif head = '-' then
+                ExplodeLine tail (List.append result [head]) ExplodeStatus.Subtract lineNumber (characterNumber + 1)
+            elif head = '*' then
+                ExplodeLine tail (List.append result [head]) ExplodeStatus.Mult lineNumber (characterNumber + 1)
+            elif head = '/' then
+                if status = Div then //not reading a string but the second time we see a dividing line...
+                    List.removeAt ((List.length result) - 1) result |> List.toArray |> System.String //it's a comment... exit and remove the previous divide character
+                else
+                    ExplodeLine tail (List.append result [head]) ExplodeStatus.Div lineNumber (characterNumber + 1) // it's could be a divide operator 
+
+            elif head = ';' then
+                ExplodeLine tail result ExplodeStatus.Semicolon lineNumber (characterNumber + 1)
+
+            elif head = '=' then
+                ExplodeLine tail (List.append result [head]) ExplodeStatus.Equal lineNumber (characterNumber + 1)
+
+            elif Char.IsLetter head then
+                ExplodeLine tail (List.append result [head]) ExplodeStatus.Letter lineNumber (characterNumber + 1)
+            else 
+                //ExplodeLine tail result status
+                raise (ValueNotUnderstoodException($"Cannot parse the unknown value in the document {head} at {lineNumber}:{characterNumber}")) // oh poop
+        elif status = ExplodeStatus.ReadString && Char.IsAscii head then
+                ExplodeLine tail (List.append result [head]) ExplodeStatus.Letter lineNumber (characterNumber + 1)
+        else
+            raise (ValueNotUnderstoodException($"Cannot parse the unknown value in the document {head} at {lineNumber}:{characterNumber}")) // oh poop
     | _ -> 
     result 
     |> List.toArray 
     |> System.String
 
-let rec ExplodeDoument (data: string list) (result : string list) : string list = 
+///<summary>Clean the doc and convert into tokens</summary>
+let rec ExplodeDocument (data: string list) (result : string list) (lineNumber: int) : string list = 
     match data with
-    | head :: tail -> 
-        let explodedLine = ExplodeLine (Seq.toList head) [] 
-        ExplodeDoument tail (List.append result [explodedLine])
+    | head :: tail ->
+        //printfn "head: %A" head
+        match head.Trim() with 
+        | "" -> ExplodeDocument tail result (lineNumber + 1) // line does not contain anything
+        | x -> 
+            if x.StartsWith("//") then 
+                ExplodeDocument tail result (lineNumber + 1) // remove lines that start with comments
+            else
+                //printfn "reading line: %A" x
+                let explodedLine = ExplodeLine (Seq.toList x) [] ExplodeStatus.Start lineNumber 0
+                ExplodeDocument tail (List.append result [explodedLine]) (lineNumber + 1)
     | _ -> result
 
-
-let ReadFile (filePath: string) =
+///<summary>Read the file and return a list of file lines</summary>
+let ReadFile (filePath: string) : string list =
     use fileReader = File.OpenRead(filePath)
     use reader = new StreamReader(fileReader)
     
@@ -156,7 +236,9 @@ let ReadFile (filePath: string) =
 
 printfn "\tStarted:\n"
 
-let doc = ReadFile(fileLocation + @"\ts.ev") |> ExplodeDoument [] |> printfn "\tResult:\n\n%A"
+let doc = ReadFile(fileLocation + @"\ts.ev") 
+
+ExplodeDocument doc [] 0 |> printfn "\tResult:\n\n%A"
 
 printfn "\n\tDone."
 
